e8391f4a04baae23788cabde5b1f2da8736aa907
User <User@DESKTOP-0CK2TV2>
2023-02-15 14:35:51 +0530

    resume task completed

---
+++ src/Variable/information.java
@@ -0,0 +1,122 @@
+package Variable; //package declaration
+
+public class information // class declaration
+{
+	//class body
+	public static void main(String[] args) //main method declaration
+	{ // Main method body
+		
+	//variable declaration
+	String name;
+	String education;
+	String emailid;
+	long mobileno;
+	String schoolname;
+	String board;
+	String Standard;
+	int yearcomp;
+	float percentage;
+	char grade;
+	String collegename;
+	String board1;
+	String Standard1;
+	int yearcomp1;
+	float percentage1;
+	char grade1;
+	String collegename1;
+	String university;
+	String Standard2;
+	int yearcomp2;
+	float percentage2;
+	char grade2;
+	String collegename2;
+	String university1;
+	String Standard3;
+	int yearcomp3;
+	float percntage3;
+	char grade3;
+	String collegename3;
+	String university2;
+	String Standard4;
+	int yearcomp4;
+	float percntage4;
+	char grade4;
+	
+	
+	//variable initialization
+	
+	name = "Priya Joshi/ Vibha Kulkarni";
+	education = "Mtech civil- environmental and LLB";
+	emailid = "vibha.o.kulkarni@gmail.com";
+	mobileno = 9922997942l;
+	schoolname = "Vidyapeeth highschool";
+	board = "SSCboard";
+	Standard = "SSC";
+	yearcomp = 1999;
+	percentage = 76.0f;
+	grade = 'A';
+	collegename = "S. M. Lohia College";
+	board1 = "HSEboard";
+	Standard1 = "HSE";
+	yearcomp1 = 2001;
+	percentage1 = 69.50f;
+	grade1 = 'A';
+	collegename1 = "KIT COE";
+	university = "Shivaji University";
+	Standard2 = "BE Environmental";
+	yearcomp2 = 2005;
+	percentage2 = 69.50f;
+	grade2 = 'A';
+	collegename2 = "Walchand college of engineering, Sangli";
+	university1 = "Shivaji University";
+	Standard3 ="Mtech Civil Environment"; 
+	yearcomp3 = 2015;
+	percntage3 = 70;
+	grade3 ='A';
+	collegename3 = "New law college, Kolhapur";
+	university2 = "Shivaji university";
+	Standard4 = "LLB";
+	yearcomp4 = 2011;
+	percntage4 = 57f;
+	grade4 = 'B';
+	
+	
+	//Variable usage
+	System.out.println("Name = " +name);
+	System.out.println("Education = "+education);
+	System.out.println("Emailid = " +emailid);
+	System.out.println("Mobile No. = " +mobileno+ " & 9511816712"+"\n"); 
+	System.out.println("School names = "+schoolname);
+	System.out.println("Board = "+board); 
+	System.out.println("Standard = "+Standard);
+	System.out.println("Yearcomp = "+yearcomp);
+	System.out.println("Percentage = "+percentage +"%");
+	System.out.println("Grade = "+ grade+"\n");
+	System.out.println("College name"+ " = "+ collegename);
+	System.out.println("Board = "+board1);
+	System.out.println("Standard = "+Standard1);
+	System.out.println("Year Completed = "+yearcomp1);
+	System.out.println("Percentage = "+percentage1+"%");
+	System.out.println("Grade = " +grade1+"\n");
+	System.out.println("College Name = "+collegename1);
+	System.out.println("University = "+university);
+	System.out.println("Branch = "+Standard2);
+	System.out.println("Passout Year = " +yearcomp2);
+	System.out.println("Percentage = "+percentage2+"%");
+	System.out.println("Grade = "+grade2+"\n");
+	System.out.println("Law college name = "+collegename3);
+	System.out.println("University = "+university2);
+	System.out.println("Degree = "+Standard4);
+	System.out.println("Passout year = "+yearcomp4);
+	System.out.println("Percentage = " +percntage4+"%");
+	System.out.println("Grade = "+grade4+"\n");
+	System.out.println("PG College Name = "+collegename2);
+	System.out.println("University = "+university1);
+	System.out.println("Degree = "+Standard3);
+	System.out.println("Passout Year = "+yearcomp3);
+	System.out.println("Percentage = "+percntage3+"%");
+	System.out.println("Grade = "+grade3+"\n");
+			
+	}
+
+}
